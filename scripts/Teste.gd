8cd1fcde6ddddf23c67685f0a94bd714c57b3060
crazynds <luli.broch@gmail.com>
2020-12-08 15:09:02 -0300

    first commit

---
+++ scripts/Teste.gd
@@ -0,0 +1,56 @@
+extends KinematicBody2D
+
+var velocity = Vector2(0,0)
+var run_speed = 350
+var jump_speed = -700
+var gravity = 2500
+var doubleJump = true
+var lado = 'dir'
+
+var auxAnimationDoubleJump = 0
+
+func _ready():
+	pass 
+
+
+func _process(delta):
+	velocity.y += gravity * delta
+	get_input()
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+
+func get_input():
+	velocity.x=0;
+	var right = Input.is_action_pressed('ui_right')
+	var left = Input.is_action_pressed('ui_left')
+	var jump = Input.is_action_just_pressed('ui_up')
+	var auxJump = doubleJump
+	
+	if left:
+		velocity.x-=run_speed
+	if right:
+		velocity.x+=run_speed
+	if is_on_floor():
+		doubleJump=true;
+		if jump:
+			velocity.y=jump_speed
+	else: if jump and doubleJump:
+		velocity.y=jump_speed
+		doubleJump=false;
+		auxAnimationDoubleJump=6
+	
+	if velocity.x>0 and lado=='esq':
+		scale.x=-1
+		lado='dir'
+	else: if velocity.x<0 and lado=='dir':
+		scale.x=-1
+		lado='esq'
+	
+	if auxAnimationDoubleJump>0:
+		auxAnimationDoubleJump=auxAnimationDoubleJump-1
+		$AnimatedSprite.animation="ocioso"
+	else: if !is_on_floor():
+		$AnimatedSprite.animation="pulando"
+	else: if velocity.x!=0:
+		$AnimatedSprite.animation="correndo"
+	else:
+		$AnimatedSprite.animation="ocioso"
