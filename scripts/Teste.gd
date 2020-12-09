fd9041d2360f32b7b8078a48d50c57af4de0a991
crazynds <luli.broch@gmail.com>
2020-12-09 16:00:43 -0300

    algumas modificacoes do som default de fala

--- scripts/Teste.gd
+++ scripts/Teste.gd
@@ -1,17 +1,12 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	$RPGLabel.print_message("Ola mundo, estou aqui apenas para testar essa box!", 24.0, ["res://sounds/sample.wav"])
-	pass # Replace with function body.
+	$RPGLabel.print_message("Ola mundo, estou aqui apenas para testar essa box!", 24.0, ["res://sounds/fala_1.wav"])
+	pass 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
